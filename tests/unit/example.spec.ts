e7cc56d9c389882c14e5f2e3815ccc0c40f60d2e
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-08-26 17:04:51 +0200

    vue testing library with jest

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,10 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/CodeBrunchCalculator.vue'
+import { render } from '@testing-library/vue'
+import About from '@/views/About.vue'
 
-describe('CodeBrunchCalculator.vue', () => {
+describe('About.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+    const about = render(About)
+    about.getByText('Code Brunch Calculator')
+    console.log(about.html())
   })
 })
