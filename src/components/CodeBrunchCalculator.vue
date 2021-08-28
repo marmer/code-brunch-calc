8aeb5249476c755ff69eff0fba5088f80ddfeeb0
MarMer <MarMer>
2021-08-29 00:10:10 +0200

    date range component experiment

--- src/components/CodeBrunchCalculator.vue
+++ src/components/CodeBrunchCalculator.vue
@@ -1,22 +1,6 @@
 <template>
-<!--  todo inner data range picker component-->
   <b-container>
-    <b-row>
-      <b-col>
-        <b-form-group label="start">
-          <b-form-datepicker
-            v-model="startDate"
-            locale="de" start-weekday="0"/>
-        </b-form-group>
-      </b-col>
-      <b-col>
-        <b-form-group label="end">
-          <b-form-datepicker
-            v-model="endDate"
-            locale="de" start-weekday="0"/>
-        </b-form-group>
-      </b-col>
-    </b-row>
+    <DateRange v-model="range"/>
     <b-form-input :value="fancyValue" type="text"></b-form-input>
     <div>
       Value: {{ fancyValue }}
@@ -26,14 +10,21 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import DateRange from '@/components/DateRange.vue'
 
-@Component
+@Component({
+  components: {
+    DateRange
+  }
+})
 export default class CodeBrunchCalculator extends Vue {
-  private startDate = '2021-01-01' // TODO: marmer 26.08.2021 First day of current year
-  private endDate = '2021-12-31' // TODO: marmer 26.08.2021 Last day of current year
+  private range = {
+    startDate: '2021-01-01', // TODO: marmer 26.08.2021 First day of current year
+    endDate: '2021-12-31' // TODO: marmer 26.08.2021 Last day of current year
+  }
 
   get fancyValue (): string {
-    return `${this.startDate} - ${this.endDate}`
+    return `${this.range.startDate} - ${this.range.endDate}`
   }
 }
 </script>
---
+++ src/components/DateRange.vue
@@ -0,0 +1,36 @@
+<template>
+<span>
+      <b-row>
+      <b-col>
+        <b-form-group label="start">
+          <b-form-datepicker
+            v-model="value.startDate"
+            locale="de" start-weekday="0"/>
+        </b-form-group>
+      </b-col>
+      <b-col>
+        <b-form-group label="end">
+          <b-form-datepicker
+            v-model="value.endDate"
+            locale="de" start-weekday="0"/>
+        </b-form-group>
+      </b-col>
+    </b-row>
+    </span>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class DateRange extends Vue {
+  @Prop() private value!: {
+    startDate: string,
+    endDate: string
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
