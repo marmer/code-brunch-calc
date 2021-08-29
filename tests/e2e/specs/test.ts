e87f69ae9561f7e010a1513747407678edc405a5
MarMer <MarMer>
2021-08-29 23:27:26 +0200

    UI test

--- tests/e2e/specs/test.ts
+++ tests/e2e/specs/test.ts
@@ -1,7 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Some Navication', () => {
+describe('App Tests', () => {
+  it('Navigation should work between about and home', () => {
     cy.visit('/')
     cy.findByText('About')
       .click()
@@ -9,15 +9,42 @@ describe('My First Test', () => {
       .should('exist')
     cy.findByText('Home')
       .click()
+    cy.findByLabelText('Start Date')
+      .should('exist')
   })
 
-  it('should be a not existing tests', async () => {
+  it('Should show all special dates within the selected range', () => {
     cy.findByLabelText('Start Date')
       .click()
-    cy.findByText(13).click()
+    cy.findAllByText(1).first().click()
     cy.findByLabelText('End Date')
       .click()
-    cy.findByText(14)
+    cy.findByText(31).last()
       .click()
+
+    cy.findByText('2021-08-06')
+      .parent()
+      .within(() => {
+        cy.findByText('Code Brunch')
+          .should('exist')
+      })
+    cy.findByText('2021-08-13')
+      .parent()
+      .within(() => {
+        cy.findByText('Code Brunch')
+          .should('exist')
+      })
+    cy.findByText('2021-08-20')
+      .parent()
+      .within(() => {
+        cy.findByText('Code Brunch')
+          .should('exist')
+      })
+    cy.findByText('2021-08-27')
+      .parent()
+      .within(() => {
+        cy.findByText('Code Brunch')
+          .should('exist')
+      })
   })
 })
