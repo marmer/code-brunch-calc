6eea9727b6a43043c07bb71a7217228978e72eef
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-09-01 14:41:35 +0200

    publishing script for github pages

--- publish.ts
+++ publish.ts
@@ -2,9 +2,9 @@ import fs from 'fs'
 
 (async () => {
   await fs.promises.copyFile('dist/index.html', 'dist/404.html')
-  await fs.promises.rm('ghpages', {
+  await fs.promises.rm('docs', {
     recursive: true,
     force: true
   })
-  await fs.promises.rename('dist', 'ghpages')
+  await fs.promises.rename('dist', 'docs')
 })()
