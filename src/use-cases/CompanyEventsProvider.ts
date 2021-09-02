48673b1b3dfff2f16ef3ee770f0da661095406e6
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-09-02 12:54:32 +0200

    SonarCloud integration

--- src/use-cases/CompanyEventsProvider.ts
+++ src/use-cases/CompanyEventsProvider.ts
@@ -4,5 +4,4 @@ export type DateRange = { startDate: Date, endDate: Date }
 
 export function getEvents (range: DateRange): CompanyEvent[] {
   return toOnlyEventsFrom(range.startDate, range.endDate)
-  // TODO: marmer 27.08.2021 Pass Exclusions (Holidays, Vacancy)
 }
