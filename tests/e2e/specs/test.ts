a61d3e2b9b0e1b4794367bf1b876cabfb627f15e
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-09-02 17:30:57 +0200

    legal holiday test

--- tests/e2e/specs/test.ts
+++ tests/e2e/specs/test.ts
@@ -88,9 +88,42 @@ describe('App Tests', () => {
     })
   })
 
+  it('should exclude legal holidays', () => {
+    cy.intercept('GET', 'https://feiertage-api.de/api?jahr=2021&nur_land=BE', {
+      statusCode: 200,
+      body: {
+        'MarMer-Day': '2021-01-17'
+      }
+    }).as('holidayCall')
+
+    cy.visit('/events?startDate=2021-09-10&endDate=2021-09-24')
+
+    cy.findByText('2021-01-17').should('exist')
+    cy.wait('@holidayCall')
+    cy.findByText('2021-01-17').should('not.exist')
+
+    cy.findByText('2021-09-10')
+      .parent()
+      .within(() => {
+        cy.findByText('Code Brunch')
+          .should('exist')
+      })
+    cy.findByText('2021-09-24')
+      .parent()
+      .within(() => {
+        cy.findByText('Code Brunch')
+          .should('exist')
+      })
+  })
+
+  // TODO: marmer 02.09.2021 Show Status while loading feiertage somehow without ad blocking ui
+  // TODO: marmer 02.09.2021 Handle broken connections to feiertage-api
+  // TODO: marmer 02.09.2021 Handle slow connections to feiertage-api
   // TODO: marmer 01.09.2021 some kind of cobra as icon
   // TODO: marmer 01.09.2021 holiday exclusions
   // TODO: marmer 02.09.2021 store allready fetched holyday exclusions
   // TODO: marmer 01.09.2021 Topics
   // TODO: marmer 01.09.2021 Ability to format the dates to the german style
+  // TODO: marmer 02.09.2021 Code coverage for Unittests
+  // TODO: marmer 02.09.2021 Code coverage for Cypresstests
 })
