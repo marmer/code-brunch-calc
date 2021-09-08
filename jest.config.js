0757191654e59ff3091e8230b982f556b4fd35cd
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-09-08 09:40:32 +0200

    coverage reports

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,9 @@ module.exports = {
   testMatch: [
     '<rootDir>/tests/unit/**/*.(test|spec).(js|jsx|ts|tsx)',
     '<rootDir>/src/**/*.(test|spec).(js|jsx|ts|tsx)'
+  ],
+  coverageReporters: [
+    'cobertura',
+    'lcov'
   ]
 }
