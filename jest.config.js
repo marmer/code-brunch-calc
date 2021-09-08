6ff4af87180cd4535240ee9337239fd7a81650ce
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-09-08 11:25:54 +0200

    some coverage configuration

--- jest.config.js
+++ jest.config.js
@@ -4,17 +4,5 @@ module.exports = {
   testMatch: [
     '<rootDir>/tests/unit/**/*.(test|spec).(js|jsx|ts|tsx)',
     '<rootDir>/src/**/*.(test|spec).(js|jsx|ts|tsx)'
-  ],
-  coverageReporters: [
-    'cobertura',
-    'lcov'
-  ],
-  coveragePathIgnorePatterns: [
-    'main.ts',
-    'src/plugins/*',
-    'src/store/index.ts',
-    'src/router/index.ts',
-    'src/main.ts',
-    'src/registerServiceWorker.ts'
   ]
 }
