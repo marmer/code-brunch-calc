bc17beb66a5fb64657f467520bdea44e04fdb074
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-09-08 15:46:16 +0200

    improved error handling

---
+++ src/use-cases/ports/holiday-api/HolidayApiClient.test.ts
@@ -0,0 +1,64 @@
+import * as underTest from './HolidayApiClient'
+
+describe('HolidayApiClient', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks()
+    jest.resetAllMocks()
+  })
+
+  it('should serve the fetched content', async () => {
+    // Preparation
+    fetchMock.mockIf('https://ipty.de/feiertag/api.php?do=getFeiertage&jahr=2019&loc=BE&outformat=Y-m-d',
+      async () => ({
+        status: 200,
+        body: '[{ "title": "Fancy Day", "date": "2019-03-04" }]'
+      }))
+
+    // Execution
+    const result = await underTest.fetchHolidays(2019)
+
+    // Assertion
+    expect(result).toStrictEqual([{
+      title: 'Fancy Day',
+      date: '2019-03-04'
+    }])
+  })
+  it('should serve an appropriate error if the response status is unexpected', async () => {
+    // Preparation
+    fetchMock.mockIf('https://ipty.de/feiertag/api.php?do=getFeiertage&jahr=2019&loc=BE&outformat=Y-m-d',
+      async () => ({
+        body: '[{ "title": "Fancy Day", "date": "2019-03-04" }]',
+        status: 403
+      }))
+
+    // Execution
+    const result = underTest.fetchHolidays(2019)
+
+    // Assertion
+    await expect(result).rejects.toStrictEqual(new Error('Error fetching holidays from API. Bad status code: 403'))
+  })
+
+  describe('should throw an appropriate error if the response has a bad structure', () => {
+    [
+      '{ "title": "Fancy Day", "date": "2019-03-04" }',
+      '[{ "bla": "blub" }]',
+      '[{ "title": "Fancy Day", 42}]',
+      '[{ "title": 42, "date": "2019-03-04" }]'
+    ].forEach(body => {
+      it(`body: ${body}`, async () => {
+        // Preparation
+        fetchMock.mockIf('https://ipty.de/feiertag/api.php?do=getFeiertage&jahr=2019&loc=BE&outformat=Y-m-d',
+          async () => ({
+            body,
+            status: 200
+          }))
+
+        // Execution
+        const result = underTest.fetchHolidays(2019)
+
+        // Assertion
+        await expect(result).rejects.toStrictEqual(new Error('Error fetching holidays from API. Response has a bad structure'))
+      })
+    })
+  })
+})
--- src/use-cases/ports/holiday-api/HolidayApiClient.ts
+++ src/use-cases/ports/holiday-api/HolidayApiClient.ts
@@ -1,9 +1,31 @@
-export async function fetchHolidays (year: number): Promise<[{ title: string, date: string }]> {
+type HolidayDTO = { title: string, date: string }
+type HolidaysDTO = [HolidayDTO]
+
+function isHolidaysDTO (body: any): body is HolidaysDTO {
+  if (!Array.isArray(body)) {
+    return false
+  }
+  for (const holiday of body) {
+    if (!('title' in holiday) ||
+      typeof holiday.title !== 'string' ||
+      !('date' in holiday) ||
+      typeof holiday.date !== 'string') {
+      return false
+    }
+  }
+  return true
+}
+
+export async function fetchHolidays (year: number): Promise<HolidaysDTO> {
   const response = await fetch(`https://ipty.de/feiertag/api.php?do=getFeiertage&jahr=${year}&loc=BE&outformat=Y-m-d`, {
     method: 'GET'
   })
   if (response.status !== 200) {
     throw new Error(`Error fetching holidays from API. Bad status code: ${response.status}`)
   }
-  return response.json()
+  const body = await response.json()
+  if (!isHolidaysDTO(body)) {
+    throw new Error('Error fetching holidays from API. Response has a bad structure')
+  }
+  return body
 }
